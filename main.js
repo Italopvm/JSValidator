9f1e36a6b21970d68f461a2a780462b25aa3b9f2
italopvm <italopvm@gmail.com>
2016-07-04 14:35:41 -0600

    second

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var validateNum = function(number){
-	if( alert(number.charAt(3) == "-" && number.charAt(7) == "-" && number.length == 12)){
+/*var validateNum = function(number){
+	if( alert(typeof number == NaN && number.charAt(3) == "-" && number.charAt(7) == "-" && number.length == 12)){
 		return true;
 	}
 	else{
@@ -8,11 +8,11 @@ var validateNum = function(number){
 }
 
 var userInput = prompt('Enter your phone number in this format: XXX-XXX-XXXX');
-validateNum(userInput);
+validateNum(userInput);*/
 
 
-var validateBDay = function(date){
-	if( alert(date.charAt(2)=='/' && date.charAt(5) == '/' && date.length == 10) ){
+/*var validateBDay = function(date){
+	if( alert( typeof date == NaN && date.charAt(2)=='/' && date.charAt(5) == '/' && date.length == 10) ){
 		return true;
 	}
 	else{
@@ -21,11 +21,11 @@ var validateBDay = function(date){
 }
 
 var userInput2 = prompt('Enter your birth date in this format: XX/XX/XXXX');
-validateBDay(userInput2);
+validateBDay(userInput2);*/
 
 
-var postalCode = function(postal){
-	if ( alert(postal.length == 5) ){
+/*var postalCode = function(postal){
+	if ( alert(typeof postal == NaN && postal.length == 5) ){
 		return true;
 	}
 	else{
@@ -33,13 +33,13 @@ var postalCode = function(postal){
     }
 }
 var userInput3 = prompt('enter your postal code in this format: XXXXX');
-postalCode(userInput3);
+postalCode(userInput3);*/
 
 
 
-// 
+//  format XXXXX or XXXXX-XXXX//
 
-var followFormat = function(format){
+/* var followFormat = function(format){
 	if (format.length == 5){
 		return alert(true);
 	}
@@ -55,6 +55,8 @@ followFormat(userInput4);
 
 
 
+// state abb//
+
 
 var stateAbb = function(abb){
 	 if (alert(abb.length == 2 && abb == abb.toUpperCase())){
@@ -66,7 +68,43 @@ var stateAbb = function(abb){
 
 }
 var userInput5 = prompt('please entre your state abbeviation using caps')
-stateAbb(userInput5);
+stateAbb(userInput5);*/
+
+
+
+// Are you married//
+
+
+/*var marriedInlower = function(married){
+	if (married.length == 3 && married == married.toLowerCase()){
+		return  alert(true);
+	}
+	else if ((married.length == 2 && married == married.toLowerCase())){
+		return alert(true);
+
+	}
+	else{
+		return alert(false);
+	}
+}
+var userInput6 = prompt('are you married enter yes or no')
+marriedInlower(userInput6);*/
+
+      //OR
+
+
+
+/*var areYouMarried = function(answer){
+	if(alert(answer.toLowerCase() == 'yes' || answer.toLowerCase() == 'no')){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+var input = prompt('Are you married? please answer yes or no')
+areYouMarried(input);*/
 
 
 
